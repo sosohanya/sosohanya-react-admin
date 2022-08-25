93c42157dda9fc46c4d38614559f892e6db91a11
sosohanya <sosohanya.com@gmail.com>
2022-08-25 21:30:20 +0900

    modal html 먼저 구성

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -5,6 +5,29 @@ import {Outlet} from "react-router-dom";
 function Layout(){
     return <div className="wrap">
         <div className="container">
+            {/* ---- modal popup 영역이 layout 영역에 있다 ---- */}
+            <div className="pop_layout" style={{zIndex:1001}}>
+                <div className="pop_inner">
+                    <div className="pop_content">
+                        <div className="pop_header">
+                            팝업 제목
+                            <a href={"/#close"} className="close"/>
+                        </div>
+                        <div className="pop_body">
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                             팝업 내용<br/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* ---- modal popup 영역이 layout 영역에 있다 ---- */}
             <div className="left_area">
                 <Lnb/>
             </div>
